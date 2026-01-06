60457ccb645e887e56d38ae65ed4d056f31b49cb
Fahim P <fahimptrippanachi@gmail.com>
2026-01-06 11:18:19 +0530

    Fix static files configuration for Render

--- student_project/settings.py
+++ student_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tk=ll=dgkwwdm7f9(0u1@1kxvlci%!y=a$7qem2wjs7iy9eq7@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'StaticFiles')]
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+#STATICFILES_DIRS=[os.path.join(BASE_DIR,'StaticFiles')]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL='media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
